2cd66f7eb71106a70f76d87e08e48110ad646c92
Fec16 <jasonkentwinata@gmail.com>
2023-09-06 17:14:00 +0700

    Final push

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Jason',
-        'class': 'PBP F'
+        'name': 'Jason Kent',
+        'class': 'PBP F',
     }
 
     return render(request, "main.html", context)
